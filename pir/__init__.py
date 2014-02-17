57b9afd13177db1c75f4aef350322f07f96eaa57
Daniel McDonald <mcdonadt@colorado.edu>
2014-02-16 23:34:13 -0700

    version bump

--- pir/__init__.py
+++ pir/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 from verman import Version
-pir_version = Version("pir", 0, 1, releaselevel="dev", init_file=__file__)
+pir_version = Version("pir", 0, 1, 1, releaselevel="dev", init_file=__file__)
 __version__ = pir_version.mmm
